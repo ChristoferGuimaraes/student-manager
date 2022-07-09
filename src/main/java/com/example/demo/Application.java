fa84d088dd4ed656f19b65df6cd9e344127ca577
Christofer Guimarães <christoferguima@gmail.com>
2022-07-09 15:42:23 -0300

    entity, repository and configuration completed

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student christofer = new Student("Christofer", "Guimarães", "christofer.guimaraes@projuris.com.br", 28);
+            studentRepository.save(christofer);
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+
+import com.example.demo.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
