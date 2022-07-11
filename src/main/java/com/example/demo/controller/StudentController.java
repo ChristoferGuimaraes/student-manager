8b0209034ac353b10ff3dd993164091080b8e241
Christofer Guimarães <christoferguima@gmail.com>
2022-07-11 12:52:08 -0300

    more crud methods

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -5,10 +5,12 @@ import com.example.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "api/student")
+@RequestMapping("/api")
 public class StudentController {
 
     private final StudentService studentService;
@@ -18,18 +20,34 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping
+    @GetMapping("students")
     public List<StudentEntity> getStudents() {
         return studentService.getStudents();
     }
 
-    @PostMapping
+    @GetMapping(path = "/student/{studentId}")
+    public Optional<StudentEntity> getStudent(@PathVariable("studentId") Long studentId) {
+        return studentService.getStudent(studentId);
+    }
+
+    @PostMapping("/student")
     public void registerNewStudent(@RequestBody StudentEntity student) {
         studentService.addNewStudent(student);
     }
 
-    @DeleteMapping(path = "{studentId}")
+    @DeleteMapping("/student/{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
     }
+
+    @PutMapping("/student/{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName,
+            @RequestParam(required = false) String email,
+            @RequestParam(required = false) LocalDate birthDate) {
+        studentService.updateStudent(studentId, firstName, lastName, email, birthDate);
+    }
+
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -5,7 +5,10 @@ import com.example.demo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,15 @@ public class StudentService {
 
     public List<StudentEntity> getStudents() {
         return studentRepository.findAll();
+    }
+    
+    public Optional<StudentEntity> getStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + "does not exists!");
+        }
+
+        return studentRepository.findById(studentId);
     }
 
     public void addNewStudent(StudentEntity student) {
@@ -38,5 +50,31 @@ public class StudentService {
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String firstName, String lastName, String email, LocalDate birthDate) {
+        StudentEntity student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exists!"));
+
+        if (firstName != null && firstName.length() > 0 && !Objects.equals(student.getFirstName(), firstName)) {
+            student.setFirstName(firstName);
+        }
+
+        if (lastName != null && lastName.length() > 0 && !Objects.equals(student.getLastName(), lastName)) {
+            student.setLastName(lastName);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<StudentEntity> studentOptional = studentRepository. findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("This e-mail is already taken!");
+            }
+            student.setEmail(email);
+        }
+
+        if (birthDate != null && !Objects.equals(student.getBirthDate(), birthDate)) {
+            student.setBirthDate(birthDate);
+        }
     }
 }
