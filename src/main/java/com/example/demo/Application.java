065b95cf6e503a8d0862ca1762c8a8f1a7d8b27f
Christofer Guimarães <christoferguima@gmail.com>
2022-07-14 20:13:53 -0300

    java version and lombok dependency

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,18 +1,42 @@
 package com.example.demo;
 
-import com.example.demo.entity.StudentEntity;
-import com.example.demo.repository.StudentRepository;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+import java.net.InetAddress;
+
+@Slf4j
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        SpringApplication app = new SpringApplication(Application.class);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            log.trace("The host name could not be determined, using localhost as fallback");
+        }
+        log.info("\n    ------------------------------------------------------\n\n\t"
+                        + "Application '{}' is running! Access URLs:\n\t"
+                        + "Local: \t\t{}://localhost:{}\n\t"
+                        + "External: \t{}://{}:{}\n\t"
+                        + "\n    ------------------------------------------------------",
+                env.getProperty("spring.application.name"), protocol, env.getProperty("server.port"), protocol,
+                hostAddress, env.getProperty("server.port"));
+
     }
+
 
 
 }
