b2e83f8b1d0399b3ff1a5f715e62fca67ef27b75
Christofer Guimarães <christoferguima@gmail.com>
2022-07-14 20:16:37 -0300

    excluded unused annotations

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,9 +3,8 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 
 import java.net.InetAddress;
