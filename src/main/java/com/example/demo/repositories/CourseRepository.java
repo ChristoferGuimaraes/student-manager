c169f0eaa323b72aed4203dfaff625d912a60db4
Christofer Guimarães <christoferguima@gmail.com>
2022-07-21 16:47:03 -0300

    change id to integer

--- src/main/java/com/example/demo/repositories/CourseRepository.java
+++ src/main/java/com/example/demo/repositories/CourseRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CourseRepository extends JpaRepository<CourseEntity, Long> {
+public interface CourseRepository extends JpaRepository<CourseEntity, Integer> {
     Optional<CourseEntity> findByName(String name);
+
 }
