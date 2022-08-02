219c694272006f1c312fdebe3105f5ab7655371b
Christofer Guimarães <christoferguima@gmail.com>
2022-08-02 20:22:19 -0300

    refactoring course service

--- src/main/java/com/example/demo/services/CourseService.java
+++ src/main/java/com/example/demo/services/CourseService.java
@@ -23,81 +23,101 @@ public class CourseService {
     private final CourseRepository courseRepository;
     private final ModelMapper modelMapper;
 
+
     private CourseDTO toCourseDTO(CourseEntity courseEntity) {
         return modelMapper.map(courseEntity, CourseDTO.class);
     }
+
 
     @Transactional(readOnly = true)
     public ResponseEntity<Object> getAllCourses(PageRequest pageRequest) {
         Page<Object> page = courseRepository.findAll(pageRequest).map(this::toCourseDTO);
 
+        if (page.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new PayloadErrorDTO("This page is empty!"));
+        }
+
         return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
+
     @Transactional(readOnly = true)
     public ResponseEntity<Object> findCourseByName(String name) {
-
         String replaceName = name.replaceAll("\\+", " ");
         Optional<CourseEntity> course = courseRepository.findByNameIgnoreCase(replaceName);
 
         if (course.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("Course '" + name + "' does not exists in database!"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new PayloadErrorDTO("Course '" + replaceName + "' does not exists in database!"));
         }
         return ResponseEntity.status(HttpStatus.OK).body(course.stream().map(this::toCourseDTO));
     }
+
 
     public ResponseEntity<Object> addNewCourse(CourseDTO courseDTO) {
         Optional<CourseEntity> course = courseRepository.findByNameIgnoreCase(courseDTO.getName());
 
         if (course.isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("Course '" + courseDTO.getName() + "' is already registered!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("Course '" + courseDTO.getName() + "' is already registered!"));
         }
 
         if (courseDTO.getName() == null || courseDTO.getName().isBlank()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("One or more fields are blank or null!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("One or more fields are blank or null!"));
         }
 
         if (courseDTO.getTeacherName() == null || courseDTO.getTeacherName().isBlank()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("One or more fields are blank or null!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("One or more fields are blank or null!"));
         }
 
         if (courseDTO.getClassNumber() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("One or more fields are blank or null!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("One or more fields are blank or null!"));
         }
 
         if (courseDTO.getStartDate() == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("One or more fields are blank or null!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("One or more fields are blank or null!"));
         }
 
         CourseEntity courseEntity = new CourseEntity(courseDTO);
         courseRepository.save(courseEntity);
 
-        return ResponseEntity.status(HttpStatus.OK).body(courseDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseDTO);
     }
 
+
     public ResponseEntity<Object> deleteCourseByName(String courseName) {
-        Optional<CourseEntity> course = courseRepository.findByNameIgnoreCase(courseName);
+        String replaceName = courseName.replaceAll("\\+", " ");
+        Optional<CourseEntity> course = courseRepository.findByNameIgnoreCase(replaceName);
 
         if (course.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("Course '" + courseName + "' does not exists in database!"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("Course '" + replaceName + "' does not exists in database!"));
         }
 
         courseRepository.deleteById(course.get().getId());
 
-        return ResponseEntity.status(HttpStatus.OK).body(new PayloadErrorDTO("Course '" + course.get().getName() + "' deleted successfully!"));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new PayloadErrorDTO("Course '" + course.get().getName() + "' deleted successfully!"));
     }
+
 
     public ResponseEntity<Object> updateCourse(String name, String courseName, String teacherName, Integer classNumber) {
         Optional<CourseEntity> courseEntity = courseRepository.findByNameIgnoreCase(name);
-        Optional<CourseEntity> courseOptional = courseRepository.findByNameIgnoreCase(courseName);
-
+        Optional<CourseEntity> courseNameParam = courseRepository.findByNameIgnoreCase(courseName);
 
         if (courseEntity.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new PayloadErrorDTO("Course '" + name + "' does not exists in database!"));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new PayloadErrorDTO("Course '" + name + "' does not exists in database!"));
         }
 
-        if (courseOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new PayloadErrorDTO("Course '" + courseName + "' already exists in database!"));
+        if (courseNameParam.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PayloadErrorDTO("Course '" + courseName + "' already exists in database!"));
         }
 
         if (courseName != null && courseName.length() > 0) {
@@ -113,6 +133,6 @@ public class CourseService {
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(toCourseDTO(courseEntity.get()));
-
     }
+
 }
