795e8010f35b2bfcd3f60f6f7fd3b8bfae403728
Christofer Guimarães <christoferguima@gmail.com>
2022-08-04 11:13:59 -0300

    removing unnecessary annotations

--- src/test/java/com/example/application/ApplicationTests.java
+++ src/test/java/com/example/application/ApplicationTests.java
@@ -1,11 +1,7 @@
 package com.example.application;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@SpringBootConfiguration
 public class ApplicationTests {
 
     @Test
